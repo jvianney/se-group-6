932d412dc304ce6d1003a48670cde6a16b759238
teaty18 <teaty5.0@gmail.com>
2015-11-23 21:43:23 +0000

    upload.php is the back-end or control part of the upload page
    work in progress

---
+++ upload.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_FILES['file']))
+{
+    $file = $_FILES['file'];
+    
+    /* Tests to see whether form works as well as file type uploaded  
+    print_r($file);
+    */
+    
+    //File properties
+    $fileName = $file['name'];
+    $fileTmp = $file['tmp_name'];
+    $fileType = $file['type'];
+    $fileSize = $file['size'];
+    
+    
+    /*white listing file extensions
+
+    This initialization stores into $fileExtention the exploaded filename, ie. puts it into an array
+   
+   */
+    
+    $fileExtension = explode('.', $fileName);
+    
+    /* tests array
+    print_r($fileExtension);
+    */
+    
+  /*converts the last element of the array which is the file extension to lower case
+  
+  */
+    
+    $fileExtension = strtolower(end($fileExtension));
+    
+    
+    
+    
+    
+    
+    
+}
