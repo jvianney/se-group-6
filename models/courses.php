34c5203d13355233238903fcf269820f1adfb0ee
jvianney <johnviann.amoah@gmail.com>
2015-11-25 17:44:40 +0000

    bug fix
    fixed faulty sql query in searchCourseByName()

--- models/courses.php
+++ models/courses.php
@@ -4,7 +4,7 @@
     class courses extends adb {
         
         function searchCourseByName($searchText) {
-            $queryText = "SELECT * FROM courses where course_title like '%$searchText'"
+            $queryText = "SELECT * FROM courses where course_title like '%$searchText%'"
             if (!$this->query($queryText)){
                 echo '{"result": 0, "message": "Error Occured While Searching For Course'.mysql_error().'"}';
                 return false;
