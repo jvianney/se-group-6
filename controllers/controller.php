51c3d263cc3c892d36942a3f7840120380ac75b1
jvianney <johnviann.amoah@gmail.com>
2015-11-25 20:54:19 +0000

    Added code to search course by name
    Added switch statement to check for command and call appropriate
    function
    
    Added function to instantiate courses class and perform necessary
    operations
    On success, a JSON array presented as a string is returned

--- controllers/controller.php
+++ controllers/controller.php
@@ -3,5 +3,41 @@
     
     if (!isset($_REQUEST['cmd'])){
         echo '{"result" : 0, "message" : "Command Not Set"}';
+        exit();
+    }
+    
+    $cmd = $_REQUEST['cmd'];
+    switch($cmd) {
+        case 1 :
+            if (!isset($_REQUEST['st'])){
+                echo '{"result" : 0, "message" : "Parameter Not Set"}';
+            } else {
+                searchCourseByName($_REQUEST['st']);
+            }
+            break;
+            
+        default :
+            echo '{"result" : 0, "message" : "Incorrect Command"}';
+            break;
+    }
+
+    function searchCourseByName($searchText) {
+        $course = new courses();
+        if (!$course->searchCourseByName($searchText)){
+            echo '{"result": 0, "message": "Error Occured While Searching For Course'.mysql_error().'"}';
+            return;
+        }
+        
+        $row = $course->fetch();
+        echo '{"result" : 1, "message" : [';
+        
+        while ($row){
+            echo json_encode($row);
+            if($row){
+                echo ",";
+            }
+        }
+        echo ']}';
+        return;
     }
 ?>
